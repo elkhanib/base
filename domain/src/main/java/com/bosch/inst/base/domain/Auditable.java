22e74dbe343451420520db51761cda1351d32ac2
XAB1SGH <Barrie.XIE@bosch-si.com>
2018-11-07 00:16:55 +0800

    add modify info to entity

---
+++ domain/src/main/java/com/bosch/inst/base/domain/Auditable.java
@@ -0,0 +1,46 @@
+package com.bosch.inst.base.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import java.util.Date;
+
+import static javax.persistence.TemporalType.TIMESTAMP;
+
+@MappedSuperclass
+@Data
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable<U> {
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    @JsonIgnore
+    protected U createdBy;
+
+    @CreatedDate
+    @Temporal(TIMESTAMP)
+    @Column(nullable = false, updatable = false)
+    @JsonIgnore
+    protected Date createdDate;
+
+    @LastModifiedBy
+    @JsonIgnore
+    protected U lastModifiedBy;
+
+    @LastModifiedDate
+    @Temporal(TIMESTAMP)
+    @JsonIgnore
+    protected Date lastModifiedDate;
+
+    // Getters and Setters
+
+}
---
+++ domain/src/main/java/com/bosch/inst/base/domain/UserAuditorBean.java
@@ -0,0 +1,36 @@
+package com.bosch.inst.base.domain;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+public class UserAuditorBean implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        SecurityContext ctx = SecurityContextHolder.getContext();
+        if (ctx == null) {
+            return null;
+        }
+        if (ctx.getAuthentication() == null) {
+            return null;
+        }
+        if (ctx.getAuthentication().getPrincipal() == null) {
+            return null;
+        }
+        Object principal = ctx.getAuthentication().getPrincipal();
+        if (principal.getClass().isAssignableFrom(User.class)) {
+            return Optional.ofNullable(((User) principal).getUsername());
+        } else {
+            return null;
+        }
+    }
+
+}
