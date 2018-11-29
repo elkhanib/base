e334e5b9c960f5a28243e629ce24bb4911259eb4
XAB1SGH <Xky19870905>
2018-11-29 14:53:45 +0800

    update spring boot to 2.1.0

--- security/src/main/java/com/bosch/inst/base/security/WebSecurityConfigurationAdapter.java
+++ security/src/main/java/com/bosch/inst/base/security/WebSecurityConfigurationAdapter.java
@@ -2,15 +2,18 @@ package com.bosch.inst.base.security;
 
 import com.bosch.inst.base.security.auth.AuthenticationProperties;
 import com.bosch.inst.base.security.auth.JwtProperties;
+import com.bosch.inst.base.security.auth.PrometheusAuthenticationProviderService;
 import com.bosch.inst.base.security.filter.CorsFilter;
 import com.bosch.inst.base.security.filter.JwtLoginFilter;
 import com.bosch.inst.base.security.filter.XRequestedHeaderFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
@@ -18,46 +21,73 @@ import org.springframework.security.web.authentication.logout.HttpStatusReturnin
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfigurationAdapter extends AbstractWebSecurityConfigurerAdapter {
+public class WebSecurityConfigurationAdapter {
     @Autowired
     private AuthenticationProperties authProperties;
 
     @Autowired
     private JwtProperties jwtProperties;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/", "/csrf", "/error", "/api/users/signup", "/swagger-ui.html", "/doc.html", "/webjars/springfox-swagger-ui/**", "/webjars/bycdao-ui/**", "/swagger-resources/**", "/v2/api-docs").permitAll()
-                .anyRequest().authenticated();
+    @Autowired
+    private PrometheusAuthenticationProviderService prometheusAuthenticationProvider;
 
+    @Configuration
+    @Order(1)
+    public class PrometheusWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
 
-        http.addFilter(new JwtLoginFilter(jwtProperties, authProperties, authenticationManager()));
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .antMatcher("/prometheus")
+                    .authenticationProvider(prometheusAuthenticationProvider)
+                    .authorizeRequests()
+                    .anyRequest().hasAnyAuthority("ACCESS", "ACTUATOR")
+                    .and()
+                    .httpBasic();
 
-        // Don't use sessions for stateless REST interfaces
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            http.csrf().disable();
+        }
 
-        // http.addFilterBefore(new EnforceSecureLoginFilter(), UsernamePasswordAuthenticationFilter.class);
-        http.addFilterBefore(new CorsFilter(), UsernamePasswordAuthenticationFilter.class);
-
-        http.addFilterBefore(getTokenAuthFilter("/**"), UsernamePasswordAuthenticationFilter.class);
-        http.addFilterBefore(getBasicAuthFilter("/**"), UsernamePasswordAuthenticationFilter.class);
-        http.addFilterBefore(getCookieAuthFilter("/**"), UsernamePasswordAuthenticationFilter.class);
-        http.addFilterBefore(new XRequestedHeaderFilter(authProperties), UsernamePasswordAuthenticationFilter.class);
-
-        http.logout()
-                .deleteCookies(authProperties.getCookie().getName())
-                .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.NO_CONTENT));
-
-        // Return the 'WWW-Authenticate: Basic' header in case of missing credentials
-        http.httpBasic();
-
-        // Hint:
-        // We disable csrf since we are running stateless REST services.
-        // Instead of the Synchronized Token Pattern we check for the presence of a custom request header.
-        // -> Only JavaScript can be used to add a custom header, and only within its origin.
-        // -> See https://www.owasp.org/index.php/Cross-Site_Request_Forgery_(CSRF)_Prevention_Cheat_Sheet
-        // Section 'Protecting REST Services: Use of Custom Request Headers'
-        http.csrf().disable();
     }
+
+    @Configuration
+    public class DefaultWebSecurityConfigurerAdapter extends AbstractWebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+
+            http.authorizeRequests().antMatchers("/", "/csrf", "/error", "/api/users/signup", "/swagger-ui.html", "/doc.html", "/webjars/springfox-swagger-ui/**", "/webjars/bycdao-ui/**", "/swagger-resources/**", "/v2/api-docs").permitAll()
+                    .anyRequest().authenticated();
+
+
+            http.addFilter(new JwtLoginFilter(jwtProperties, authProperties, authenticationManager()));
+
+            // Don't use sessions for stateless REST interfaces
+            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+            // http.addFilterBefore(new EnforceSecureLoginFilter(), UsernamePasswordAuthenticationFilter.class);
+            http.addFilterBefore(new CorsFilter(), UsernamePasswordAuthenticationFilter.class);
+
+            http.addFilterBefore(getTokenAuthFilter("/**"), UsernamePasswordAuthenticationFilter.class);
+            http.addFilterBefore(getBasicAuthFilter("/**"), UsernamePasswordAuthenticationFilter.class);
+            http.addFilterBefore(getCookieAuthFilter("/**"), UsernamePasswordAuthenticationFilter.class);
+            http.addFilterBefore(new XRequestedHeaderFilter(authProperties), UsernamePasswordAuthenticationFilter.class);
+
+            http.logout()
+                    .deleteCookies(authProperties.getCookie().getName())
+                    .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.NO_CONTENT));
+
+            // Return the 'WWW-Authenticate: Basic' header in case of missing credentials
+            http.httpBasic();
+
+            // Hint:
+            // We disable csrf since we are running stateless REST services.
+            // Instead of the Synchronized Token Pattern we check for the presence of a custom request header.
+            // -> Only JavaScript can be used to add a custom header, and only within its origin.
+            // -> See https://www.owasp.org/index.php/Cross-Site_Request_Forgery_(CSRF)_Prevention_Cheat_Sheet
+            // Section 'Protecting REST Services: Use of Custom Request Headers'
+            http.csrf().disable();
+        }
+    }
+
 
 }
---
+++ security/src/main/java/com/bosch/inst/base/security/auth/PrometheusAuthenticationProviderService.java
@@ -0,0 +1,72 @@
+package com.bosch.inst.base.security.auth;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+/**
+ * Custom authentication provider used to authenticate the /prometheus endpoint user. <br>
+ * This releases IoT permissions form a large number of basic auth authentication requests.
+ */
+@Component
+@Slf4j
+public class PrometheusAuthenticationProviderService implements AuthenticationProvider {
+
+    @Value("${prometheus.user:prometheus.user.unkonwn}")
+    private String prometheusUser;
+
+    @Value("${prometheus.password:prometheus.password.unknown}")
+    private String prometheusPassword;
+
+    private UsernamePasswordAuthenticationToken prometheusAuthenticationToken;
+
+    public PrometheusAuthenticationProviderService() {
+        createPrometheusToken();
+    }
+
+    private void createPrometheusToken() {
+        SimpleGrantedAuthority access = new SimpleGrantedAuthority("ACCESS");
+        SimpleGrantedAuthority actuator = new SimpleGrantedAuthority("ACTUATOR");
+        List<SimpleGrantedAuthority> authorities = asList(access, actuator);
+        prometheusAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(prometheusUser, prometheusPassword, authorities);
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) {
+        if (checkCredentials(authentication)) {
+            log.debug("User {} authenticated. Provide authority 'ACCESS' and 'ACTUATOR'", authentication.getName());
+            return prometheusAuthenticationToken;
+        } else {
+            log.debug("User {} unknown", authentication.getName());
+            return null;
+        }
+    }
+
+    private boolean checkCredentials(Authentication authentication) {
+        String nameToCheck = authentication.getName();
+        String passwordToCheck = authentication.getCredentials().toString();
+        if (nameToCheck.equals(prometheusUser)) {
+            if (passwordToCheck.equals(prometheusPassword)) {
+                return true;
+            } else {
+                log.warn("User {} found but password does not match!", nameToCheck);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+
+}
